e674c7d8a5cd03a49447c208d24b64aea350a048
Unknown <viherbos@eln.upv.es>
2018-02-22 19:12:25 +0100

    SOS

--- batch_temp.sh
+++ batch_temp.sh
@@ -1,5 +1,53 @@
-for i in {1..60}
-do
+python socket_client.py -r
+
+python socket_client.py -d V2 12
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 11
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 10
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 9
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 8
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 7
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 6
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 5
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
+python socket_client.py -d V2 4
+sleep 2400
+python socket_client.py -a 900 temp_sweep
+sleep 910
+
 python socket_client.py -t
 sleep 60
-done
+
+wait $!
+python socket_client.py -f temp_sweep 10
+
--- batch_test.sh
+++
@@ -1,5 +0,0 @@
-./socket_client.py -d ON
-./socket_client.py -a 5 DC_test
-./socket_client.py -f DC_test 1
-./socket_client.py -d OFF
-
